ed512249781fb19e753d0c92bbf3aab7443df83b
Manos Vasilopoulos <emmanouilvasilopoulos@gmail.com>
2026-01-30 09:05:57 +0000

    cosmetics

--- exps/default/yolox_m_v2.py
+++ exps/default/yolox_m_v2.py
@@ -5,7 +5,6 @@
 import os
 
 from yolox.exp import ExpV2
-
 """
 according to ExpV2:
 - input_size is used to set both test_size and input_size
@@ -17,10 +16,12 @@ according to yolox/data/datasets/datasets_wrapper.py
 - input_dimension (tuple): (width,height) tuple with default dimensions of the network
 """
 
+
 class Exp(ExpV2):
+
     def __init__(self, input_width=640, input_height=640):
         super(Exp, self).__init__((input_width, input_height))
         self.depth = 0.67
         self.width = 0.75
-        self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
-        
+        self.exp_name = os.path.split(
+            os.path.realpath(__file__))[1].split(".")[0]
