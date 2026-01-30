0266d97e55ed0f702427bb4f32953351044019f2
Manos Vasilopoulos <emmanouilvasilopoulos@gmail.com>
2026-01-30 09:04:38 +0000

    add instructions for running docker container

---
+++ run-docker-command-dgx-spark.sh
@@ -0,0 +1,3 @@
+docker run -dit --gpus all --shm-size=128g --name yolox_container -v /home/manos/tools/YOLOX:/home/YOLOX yolox_infra_torch210:latest tail -f /dev/null
+# you can then exec into the container with:
+docker exec -it yolox_container /bin/bash
