d7826b77dc1c3c34846f365df9726071c7e029c3
Manos Vasilopoulos <emmanouilvasilopoulos@gmail.com>
2026-02-01 14:29:57 +0000

    import coco evaluator v2

--- yolox/evaluators/__init__.py
+++ yolox/evaluators/__init__.py
@@ -3,4 +3,5 @@
 # Copyright (c) Megvii, Inc. and its affiliates.
 
 from .coco_evaluator import COCOEvaluator
+from .coco_evaluator_v2 import COCOEvaluatorV2
 from .voc_evaluator import VOCEvaluator
