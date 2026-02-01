0653b7678d1cb859736b151bb4d5071f0bafb545
Manos Vasilopoulos <emmanouilvasilopoulos@gmail.com>
2026-02-01 14:30:45 +0000

    enforce FP32 precision model output (not tested)

--- yolox_manos/export_yolox.py
+++ yolox_manos/export_yolox.py
@@ -3,7 +3,8 @@ import exps.default.yolox_m
 from yolox.utils import replace_module
 from yolox.models.network_blocks import SiLU
 
-EXPORT_INPUT_SIZE = (640, 640) # (height, width)
+EXPORT_INPUT_SIZE = (640, 640)  # (height, width)
+
 
 def main():
     exp = exps.default.yolox_m.Exp()
@@ -17,8 +18,11 @@ def main():
     model = replace_module(model, torch.nn.SiLU, SiLU)
     model.head.decode_in_inference = False
     dummy_input = torch.randn(1, 3, EXPORT_INPUT_SIZE[0], EXPORT_INPUT_SIZE[1])
-    output = model(dummy_input)
-    print(output.shape)
+    outputs = model(dummy_input)
+    predictions = outputs["predictions"]
+    low_res_feats = outputs["low_res_feats"]
+    mid_res_feats = outputs["mid_res_feats"]
+    high_res_feats = outputs["high_res_feats"]
     """ ------------ ONNX Export ------------- """
     # input_names = ["input"]
     # output_names = ["output"]
@@ -42,8 +46,9 @@ def main():
     mlmodel = ct.convert(
         traced_model,
         inputs=[ct.TensorType(name="input", shape=dummy_input.shape)],
-    )
-    mlmodel.save(f"yolox_m_{EXPORT_INPUT_SIZE[0]}x{EXPORT_INPUT_SIZE[1]}.mlpackage")
+        compute_precision=ct.precision.FLOAT32)
+    mlmodel.save(
+        f"yolox_m_{EXPORT_INPUT_SIZE[0]}x{EXPORT_INPUT_SIZE[1]}.mlpackage")
 
 
 if __name__ == "__main__":
